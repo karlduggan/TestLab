4a7a4b7b1fed2d3410388d8b24ec0805fa31bcfd
Karl Duggan <karlduggan_55@hotmail.com>
2022-10-11 09:01:54 +0100

    Update AddBugComponent.vue

--- frontend/src/components/AddBugComponent.vue
+++ frontend/src/components/AddBugComponent.vue
@@ -1,18 +1,19 @@
 <template>
     <div class="add-bug-container">
         <h3>Title:</h3>
-        <input type="text">
+        <input v-model="title_input" type="text">
         <h4>Description:</h4>
-        <input type="text" name="" id="">
+        <input v-model="description_input" type="text" name="" id="">
         <h4>Steps to Reproduce</h4>
         <div v-for="(step, index) in this.steps " :key="step">
             {{index + 1}}: {{step}}
         </div>
         <input v-model="step_line" type="text" @keyup.enter="addStep">
         <h4>Expected Results:</h4>
-        <input type="text" name="" id="">
+        <input v-model="expected_input" type="text" name="" id="">
         <h4>Actual Results:</h4>
-        <input type="text" name="" id="">
+        <input v-model="actual_input" type="text" name="" id="">
+        <button @click="addBug" >Add</button>
     </div>
 </template>
 <script>
@@ -20,8 +21,12 @@
         name: "AddBugComponent",
         data(){
             return {
+                title_input: "",
+                description_input: "",
                 step_line: "",
-                steps: []
+                steps: [],
+                expected_input: "",
+                actual_input: ""
             }
         },
         methods: {
@@ -29,7 +34,18 @@
                 console.log("Step added")
                 this.steps.push(this.step_line)
                 this.step_line = ""
-            }
+            },
+            addBug: function(){
+                let bug_payload = {
+                "title": this.title_input, 
+                "description": this.description_input,
+                "steps": this.steps,
+                "expected": this.expected_input,
+                "actual": this.actual_input}
+
+                this.$store.state.selectedBugList.push(bug_payload);
+            },
+            
         }
     }
 </script>
