e02ddbb2f88dcd6a8b6eafcb5c980ef3833d4144
“Karl <karlduggan_55@hotmail.com>
2022-10-13 17:33:05 +0100

    Update AddBugComponent.vue

--- frontend/src/components/AddBugComponent.vue
+++ frontend/src/components/AddBugComponent.vue
@@ -1,18 +1,18 @@
 <template>
     <div class="add-bug-container">
         <h3>Title:</h3>
-        <input v-model="title_input" type="text">
+        <input v-model="title_input" type="text" class="input-line">
         <h4>Description:</h4>
-        <input v-model="description_input" type="text" name="" id="">
+        <input v-model="description_input" type="text" name="" class="input-line">
         <h4>Steps to Reproduce</h4>
         <div v-for="(step, index) in this.steps " :key="step">
             {{index + 1}}: {{step}}
         </div>
-        <input v-model="step_line" type="text" @keyup.enter="addStep">
+        <input v-model="step_line" type="text" @keyup.enter="addStep" class="input-line">
         <h4>Expected Results:</h4>
-        <input v-model="expected_input" type="text" name="" id="">
+        <textarea v-model="expected_input" type="text" name="" class="text-area" rows="4"></textarea>
         <h4>Actual Results:</h4>
-        <input v-model="actual_input" type="text" name="" id="">
+        <textarea v-model="actual_input" type="text" name="" class="text-area" rows="4"></textarea>
         <button @click="addBug">Add</button>
     </div>
 </template>
@@ -49,11 +49,42 @@
                 }
 
                 this.$store.state.selectedBugList.push(bug_payload);
+                // Close modal
+                this.$store.state.showModal = false;
+                // Clear all inputs 
+                this.title_input = ""
+                this.description_input = ""
+                this.step_line = ""
+                this.steps = []
+                this.expected_input = ""
+                this.actual_input = ""
             },
             
         }
     }
 </script>
 <style scoped>
+.input-line {
+    width: 100%;
+    background-color: rgb(243, 243, 243);
+    border: none;
+    padding: 5px;
+    box-sizing: border-box;
+}
+.input-line:focus {
+    outline: none;
+}
+
+.text-area {
+    background-color: rgb(243, 243, 243);
+    border: none;
+    width: 100%;
+    padding: 5px;
+    resize: none;
+    box-sizing: border-box;
+}
+.text-area:focus {
+    outline: none;
+}
 
 </style>
